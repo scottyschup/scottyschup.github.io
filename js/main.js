ad206c50092f423c17d6a22ff9d94dc5bebdbda5
scottyschup <ssschupbach@gmail.com>
2015-05-01 01:53:20 -0700

    Work on personal site animations

--- js/main.js
+++ js/main.js
@@ -36,9 +36,72 @@ $(function () {
     width: "125px"
   });
 
-  $(window).scroll(function (event) {
-    var st = $(this).scrollTop();
-    var delta = -st / 5;
-    $(".background").css("transform", "translateY(" + delta + "px)");
-  });
+  if (window.SSdotCom === undefined) {
+    window.SSdotCom = {};
+  }
+
+  SSdotCom.animatePage = function () {
+    SSdotCom.contactLeft = $(".contact hgroup").position().left;
+    SSdotCom.sectionsLeft = $(".sections").position().left;
+
+    $(window).scroll(function (event) {
+      var st = $(this).scrollTop();
+      var percComplete = (1 - (300 - st) / 225);
+      var yDelta = -st / 5;
+      $(".background").css("transform", "translateY(" + yDelta + "px)");
+
+      if (st > 75 && st < 300) {
+        // position fixed; translate
+        var newContactLeft = SSdotCom.contactLeft * (1 - percComplete);
+        $(".contact hgroup").css({
+          "position": "fixed"
+        });
+
+        $(".contact hgroup").css({
+          "left": newContactLeft + "px",
+          "top": "46px"
+        });
+        var newSectionsLeft = SSdotCom.sectionsLeft +
+          (SSdotCom.sectionsLeft * percComplete);
+        $(".sections").css({
+          "position": "absolute",
+          "left": newSectionsLeft + "px",
+          "top": (75 - st) + "px"
+        })
+      }
+
+      if (st < 75) {
+        // position
+        $(".contact hgroup, .sections").css({
+          "position": "static"
+        });
+
+        $(".contact hgroup").css({
+          "background": "none"
+        });
+      }
+
+      if (st > 75) {
+        $(".contact hgroup").css({
+          "background-color": "rgba(140, 175, 210, 0.85)"
+        });
+      }
+
+      if (st > 300) {
+        $(".contact hgroup").css({
+          "left": "0px",
+          "top": "46px",
+          "background-color": "#7aa",
+          "box-shadow": "0 0 10px #7aa",
+          "margin-left": "-10px",
+          "padding-left": "20px"
+        });
+
+        $(".sections").css({
+          "left": (SSdotCom.sectionsLeft * 2) + "px",
+          // "top": (75 - st) + "px"
+        })
+      }
+    });
+  };
 }());
