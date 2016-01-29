73d84e5516756804ad6d190996432b50781b9409
Scott Schupbach <sschupbach@asperasoft.com>
2016-01-28 17:07:56 -0800

    Refactor initialization function

--- js/main.js
+++ js/main.js
@@ -19,16 +19,18 @@ $(function () {
   };
 
   SSdotCom.prototype.startParallaxScroll = function () {
-    $(window).scroll(function () {
-      var st = $(this).scrollTop();
-      var yDelta = -st / 5;
-      $(".background").css("transform", "translateY(" + yDelta + "px)");
-    });
+    $(window).scroll(this.pScroll);
   };
 
-  SSdotCom.narrowGap = function ($el) {
-
+  SSdotCom.prototype.pScroll = function () {
+    var st = $(this).scrollTop();
+    var yDelta = -st / 5;
+    $(".background").css("transform", "translateY(" + yDelta + "px)");
   };
+
+  // SSdotCom.narrowGap = function ($el) {
+  //
+  // };
 
   // SSdotCom.dropDownNav = function () {
   //   $(".page-nav").css("top", "100px");
